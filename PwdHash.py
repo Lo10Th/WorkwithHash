2111829bdc32142154dded405f12befaabd233cc
Lo10Th <70955799+Lo10Th@users.noreply.github.com>
2022-04-03 20:03:05 +0200

    Update PwdHash.py

--- PwdHash.py
+++ PwdHash.py
@@ -1,9 +1,13 @@
 import hashlib
 from time import sleep
+#Here we ask for the password which the computer should hash
 pwd = input("Which password would you like to choose? Enter here:   ")
+#The entered is hashed and saved as variable 'pwdhash'.
 pwdhash = hashlib.md5(pwd.encode('utf-8')).hexdigest()
 sleep(2)
+#We ask the user for his own password from before
 pwdrequest = input("What is your password?  Enter here:   ")
+#The requested password is hashed and then compared with the correct password
 pwdrequesthash = hashlib.md5(pwdrequest.encode('utf-8')).hexdigest()
 if pwdrequesthash == pwdhash:
     print("Login successful!")
